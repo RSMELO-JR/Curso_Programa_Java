78839d6fe9505bb3fc44fa4f4d4ecd5eeb1ebc98
RSMELO-JR <rsmelojr@yahoo.com>
2022-11-04 20:27:53 -0300

    novo comando utilizando a integração com o eclipse
    Signed-off-by: RSMELO-JR <rsmelojr@yahoo.com>

--- Estrutura01_Sequencial/Main01_Ola.java
+++ Estrutura01_Sequencial/Main01_Ola.java
@@ -5,7 +5,7 @@ public class Main01_Ola {
 	public static void main(String[] args) {
 		
 		System.out.println("Olá Mundo");
-		System.out.println("Hallo, World!");
+		System.out.println("Hello, World!");
 	}
 
 }
